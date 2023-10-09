445a44503f31c990738c605bedfb5e43fd44cd9f
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-09 17:50:27 +0200

    ADD - first snack

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+let access = false;
+
 // - Chiedere all'utente tramite un prompt la sua email
 
 let userEmail 
@@ -6,5 +9,27 @@ userEmail = prompt ("inserisci la tua email")
 console.log (userEmail)
 
 
+// - Controllare che sia nella lista di chi può accedere
+
+//      - Dichiarare l'array con l'email accettate
+const emailAccepted = ["mario@gmail.com", "luca@gmail.com", "gianpiero@gmail.com"];
 
 
+//      - Dichiarare il ciclo di controllo dell'email
+const lenghtArray = emailAccepted.length
+
+for (let i = 0; i < lenghtArray; i++){
+    let emailSelected = emailAccepted[i] 
+
+//      - SE l'email rientra tra quelle accettate
+    if (userEmail === emailSelected){
+        access = true
+    }
+}
+
+if (access === true){
+    console.log("Email corretta")
+}
+else{
+    console.log("Email sbagliata")
+}
